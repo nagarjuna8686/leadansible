edda0d85668a4cd948f3b29377de00400ad04fbc
nagarjuna8686 <34602673+nagarjuna8686@users.noreply.github.com>
2019-10-16 10:15:20 +0530

    Update install.sh

--- roles/install.sh
+++ roles/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo rm -R /home/ubuntu/tomcat7/webapps/leadapp
-sudo rm /home/ubuntu/tomcat7/webapps/leadapp.war
+sudo apt-get install nginx -y
+#sudo rm -R /home/ubuntu/tomcat7/webapps/leadapp
+#sudo rm /home/ubuntu/tomcat7/webapps/leadapp.war
