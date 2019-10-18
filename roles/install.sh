ecc1eeb897ea1290e778639ee135b87272589da1
nagarjuna8686 <34602673+nagarjuna8686@users.noreply.github.com>
2019-10-18 10:20:18 +0530

    Update install.sh

--- roles/install.sh
+++ roles/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get install mysql-server -y
-mysql -h leadappdb.c15xm7hypcsn.us-east-2.rds.amazonaws.com -P 3306 -u lead -p leadapp</home/ubuntu/schema.sql
+mysql -h leadappdb.c15xm7hypcsn.us-east-2.rds.amazonaws.com -P 3306 -u lead -p leadapp123 leadapp</home/ubuntu/schema.sql
 #sudo rm -R /home/ubuntu/tomcat7/webapps/leadapp
 #sudo rm /home/ubuntu/tomcat7/webapps/leadapp.war
